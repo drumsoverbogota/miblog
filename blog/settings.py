2d109efb5fc9d80660c4a6a7ff81d472ed584e22
drumsoverbogota <sergiomanceranom@gmail.com>
2021-03-28 22:15:20 -0500

    [BLOG-002]
    - Actualizacion para usar varios proyectos

--- blog/settings.py
+++ blog/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/blogdjango/static/'
+SESSION_COOKIE_NAME = 'blogdjango'
 
 LOGIN_REDIRECT_URL = '/'
 
